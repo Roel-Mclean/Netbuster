b00685f1d557b3a88c7752ede886b03e42011c0d
Isaac Hussain <hussainisaac@gmail.com>
2023-12-10 21:44:54 +0000

    Admin page built

---
+++ the-drop-shop-admin-new/the-drop-shop-admin/src/components/footer.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components"
+
+const StyledFooter = styled.footer`
+  width: 100%;
+  height: 90px;
+  line-height: 80px;
+  background-color: #1F1F1F;
+  position: absolute; // changed to absolute
+  bottom: 0;
+  left: 0; // ensure it's aligned left
+  text-align: center;
+  font-size: 32px;
+  color: #FFF;
+  margin-top: 0;
+
+  @media (min-height: 100vh) {
+    position: fixed; // fixed for screens with at least 100vh height
+  }
+`;
+
+export const Footer = () => {
+    return (
+        <StyledFooter>Netbuster</StyledFooter>
+    );
+}
